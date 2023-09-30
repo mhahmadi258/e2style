d5709663126f7d2af46fa3995f52bb930ad92f27
Mohammad Hossein Ahmadi <mh.ahmadi258@gmail.com>
2023-09-30 05:04:00 +0330

    Create l2 regularization

--- training/coach.py
+++ training/coach.py
@@ -233,6 +233,10 @@ class Coach:
 			loss_w_norm = self.w_norm_loss(latent, self.net.latent_avg)
 			loss_dict['loss_w_norm'] = float(loss_w_norm)
 			loss += loss_w_norm * self.opts.w_norm_lambda
+		if self.opts.l2_regularization >0:
+			learnable_parameters = torch.cat([x.view(-1) for x in self.net.parameters() if x.requires_grad==True])
+			print(learnable_parameters.shape)
+			loss += torch.norm(learnable_parameters, 2) * self.opts.l2_regularization
 		loss_dict['loss'] = float(loss)
 		return loss, loss_dict, id_logs
 
