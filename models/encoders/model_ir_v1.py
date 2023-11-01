13e049340f1126ebaf943d2d639d63f0cb63e264
Mohammad Hossein Ahmadi <mh.ahmadi258@gmail.com>
2023-11-01 19:39:47 +0330

    Handle facenet bug

--- models/encoders/model_ir_v1.py
+++ models/encoders/model_ir_v1.py
@@ -246,6 +246,7 @@ class InceptionResnetV1(nn.Module):
         self.dropout = nn.Dropout(dropout_prob)
         self.last_linear = nn.Linear(1792, 512, bias=False)
         self.last_bn = nn.BatchNorm1d(512, eps=0.001, momentum=0.1, affine=True)
+        self.logits = nn.Linear(512, tmp_classes)
 
 
 
