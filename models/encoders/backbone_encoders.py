0dd5e49266afe9681f729c354421cb0d9e153950
Mohammad Hossein Ahmadi <mh.ahmadi258@gmail.com>
2023-11-26 04:21:31 +0330

    Init spatial adapter with 6 block

--- models/encoders/backbone_encoders.py
+++ models/encoders/backbone_encoders.py
@@ -18,6 +18,8 @@ class AdapterBlock(Module):
 
     def forward(self, x , w):
         out = self.adapter(x).view(-1, self.num_module, 512)
+        for i in range(self.num_module):
+            w = w[:,i,...] + out[:,i,...]
         return w + out
 
 class BackboneEncoderFirstStage(Module):
@@ -40,21 +42,19 @@ class BackboneEncoderFirstStage(Module):
                                          Flatten(),
                                          Linear(256 * 7 * 7, 512 * 9))
         
-        self.adapter_layer_3 = AdapterBlock(256, 9)
+        self.adapter_layer_3 = AdapterBlock(256, 6)
         
         self.output_layer_4 = Sequential(BatchNorm2d(128),
                                          torch.nn.AdaptiveAvgPool2d((7, 7)),
                                          Flatten(),
                                          Linear(128 * 7 * 7, 512 * 5))
         
-        self.adapter_layer_4 = AdapterBlock(128, 5)
         
         self.output_layer_5 = Sequential(BatchNorm2d(64),
                                          torch.nn.AdaptiveAvgPool2d((7, 7)),
                                          Flatten(),
                                          Linear(64 * 7 * 7, 512 * 4))
         
-        self.adapter_layer_5 = AdapterBlock(64, 4)
         
         modules = []
         for block in blocks:
@@ -70,11 +70,9 @@ class BackboneEncoderFirstStage(Module):
         for l in self.modulelist[:3]:
           x = l(x)
         lc_part_4 = self.output_layer_5(x).view(-1, 4, 512)
-        lc_part_4 = self.adapter_layer_5(x, lc_part_4)
         for l in self.modulelist[3:7]:
           x = l(x)
         lc_part_3 = self.output_layer_4(x).view(-1, 5, 512)
-        lc_part_3 = self.adapter_layer_4(x, lc_part_3)
         for l in self.modulelist[7:21]:
           x = l(x)
         lc_part_2 = self.output_layer_3(x).view(-1, 9, 512)
